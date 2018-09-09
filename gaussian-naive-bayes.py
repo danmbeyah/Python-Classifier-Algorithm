c91187a7c0586ba603765f1b47b93bed630fba80
Daniel Mbeyah <danmbeyah@gmail.com>
2018-09-09 10:40:44 +0300

    Update and rename naive-bayes.py to gaussian-naive-bayes.py

--- gaussian-naive-bayes.py
+++ gaussian-naive-bayes.py
@@ -12,12 +12,6 @@ labels = data['target']
 feature_names = data['feature_names']
 features = data['data']
 
-# Look at our data
-print(label_names)
-print('Class label = ', labels[0])
-print(feature_names)
-print(features[0])
-
 # Split our data
 train, test, train_labels, test_labels = train_test_split(features,
                                                           labels,
